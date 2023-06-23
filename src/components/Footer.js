82871c43dcbc52bf1cbb66c072302ff8218f43ce
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2023-06-23 16:02:10 +0200

    kasa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
-import"../styles/index.css"
-function Footer(){
-    return(
-        <footer className="footer-container">
-            <img src={require("../assets/LOGOnoiretblanc.png")} className="footer-img" alt="logo" />
-            <p className="footer-text">2020 Kasa. All rights reserved </p>
-        </footer>
-    )
+import "../styles/index.css";
+function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="footer-center">
+        <img
+          src={require("../assets/LOGOnoiretblanc.png")}
+          className="footer-img"
+          alt="logo"
+        />
+        <p className="footer-text">2020 Kasa. All rights reserved </p>
+      </div>
+    </footer>
+  );
 }
-export  default Footer
+export default Footer;
