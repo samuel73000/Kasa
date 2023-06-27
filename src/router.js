994755ba85f5edc572af99472de751631e16da3c
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2023-06-27 20:48:49 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Propos from "./pages/Propos";
-import Logement from "./pages/Logement";
-import Error from "./pages/Error";
+import Home from "./pages/home";
+import Propos from "./pages/propos";
+import Logement from "./pages/logement";
+import Error from "./pages/error";
 import App from "./App";
 
 const router = createBrowserRouter([
